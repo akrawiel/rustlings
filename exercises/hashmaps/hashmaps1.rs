194ffdcf35b4577cbc189b9bb12e39b25904d6f4
Aleksander Krawiel <aleksander.krawiel@makimo.pl>
2023-10-10 17:48:41 +0200

    Initial work on exercises

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -11,17 +11,20 @@
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new(); // TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
+
+    basket.insert(String::from("1"), 1);
+    basket.insert(String::from("2"), 1);
+    basket.insert(String::from("3"), 1);
+    basket.insert(String::from("4"), 1);
 
     basket
 }
--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,10 +8,15 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
+}
+
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        // self.iter().cloned().chain(vec!["Bar".into()]).collect()
+        [self, vec!["Bar".into()]].concat()
+    }
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
